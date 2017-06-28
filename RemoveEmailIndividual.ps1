6eda957075ba36d68f503337f065d5b8bde9109d
Kesrich <ken.e.stevens@gmail.com>
2017-06-28 12:27:11 -0400

    Add files via upload

---
+++ RemoveEmailIndividual.ps1
@@ -0,0 +1,5 @@
+﻿#Remove from individual mailbox all copies
+#Search-Mailbox -Identity "tom_reedy@carmax.com" -SearchQuery 'Subject:"CPL update - closing"' -DeleteContent -Confirm
+
+#Remove from individual mailbox within a time period
+Search-Mailbox -Identity "bill_nash@carmax.com" -SearchQuery 'Subject:"CPL update - closing" sent:2017/6/22..2017/6/23 ' -DeleteContent -Confirm
---
+++ connect0365.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module MSOnline
+$O365Cred = Get-Credential
+
+#use account kstevens-cl@carmax.com
+
+$proxysettings = New-PSSessionOption -ProxyAccessType IEConfig
+$O365Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $O365Cred -Authentication Basic -AllowRedirection -SessionOption $proxysettings
+Import-PSSession $O365Session -AllowClobber
+Connect-MsolService –Credential $O365Cred 
+
+#run a get-mailbox of someone else to verify you connected!
+get-mailbox jseaborne-cl@carmax.com
+
+#disconnect PS-Session (copy and paste it to command line to execute to kill session...)
+#Remove-PSSession $O365Session
